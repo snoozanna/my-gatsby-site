fc75c286548de766411e04a226d67d619ecdd7de
suzannahurst <hurstsuzanna@gmail.com>
2023-05-11 15:52:30 +0100

    build

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,15 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
 module.exports = {
   siteMetadata: {
     title: `My Gatsby Site`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
-}
+  plugins: [`gatsby-plugin-appointlet`],
+};
